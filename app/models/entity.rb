c4b1d8080ef90fcacafbc879f9511a35865b08d4
Htetaungkyaw71 <htetaung200071@gmail.com>
2023-03-27 16:24:10 +0630

    added all migrations

---
+++ app/models/entity.rb
@@ -0,0 +1,5 @@
+class Entity < ApplicationRecord
+    belongs_to :user
+    has_many :group_entities , dependent: :destroy
+    has_many :entities, through: :group_entities
+end
---
+++ app/models/group.rb
@@ -0,0 +1,5 @@
+class Group < ApplicationRecord
+    belongs_to :user
+    has_many :group_entities , dependent: :destroy
+    has_many :groups, through: :group_entities  
+end
