2853b5a326e118e1a54e0c0842c9820615a7decc
Htetaungkyaw71 <htetaung200071@gmail.com>
2023-03-29 13:03:04 +0630

    Added styling for project

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,6 +4,7 @@ class GroupsController < ApplicationController
   # GET /groups or /groups.json
   def index
     @groups = Group.where(user_id: current_user.id)
+    @username = current_user.name
   end
 
   # GET /groups/1 or /groups/1.json
