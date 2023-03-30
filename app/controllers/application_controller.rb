f2e4cbca208d04d74618eabd5166cb19bf11053c
Htetaungkyaw71 <htetaung200071@gmail.com>
2023-03-30 20:56:47 +0630

    Fix home page errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, [:home] unless Rails.env == 'test'
+  before_action :authenticate_user!, except: [:home] unless Rails.env == 'test'
   protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
