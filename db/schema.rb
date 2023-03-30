b4a4cdab83f40af89c32a98893aee3b8131cfa48
Htetaungkyaw71 <htetaung200071@gmail.com>
2023-03-30 11:07:16 +0630

    delete db and create db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_28_090726) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_30_043619) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_090726) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.bigint "group_id", null: false
+    t.index ["group_id"], name: "index_entities_on_group_id"
     t.index ["user_id"], name: "index_entities_on_user_id"
   end
 
@@ -45,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_090726) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "entities", "groups"
   add_foreign_key "entities", "users"
   add_foreign_key "groups", "users"
 end
